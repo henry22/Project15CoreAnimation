bfd466e0511f8c8b48a7346eb6c7fcb8c8a5ad75
henry22 <henry811010@gmail.com>
2015-06-22 17:16:09 +0800

    animateWithDuration(), it actually accepts two closures. The parameters be using are how to long animate for, how long to pause before the animation starts, any options you want to provide, what animations to execute, and finally a closure that will execute when the animation finishes.

--- Project15CoreAnimation/ViewController.swift
+++ Project15CoreAnimation/ViewController.swift
@@ -29,6 +29,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapped(sender: AnyObject) {
+        //When the method begins, hide the tap button so that the animations don't collide
+        tap.hidden = true
+        
+        //call animateWithDuration() with a duration of 1 second, no delay, and no interesting options
+        UIView.animateWithDuration(1, delay: 0, options: .allZeros,
+            //For the animations closure first do the [unowned self] in to avoid strong reference cycles, then enter the switch/case code
+            animations: { [unowned self] in
+            //switch using the value of self.currentAnimation, use self to make the closure capture clear
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+                //use trailing closure syntax to provide the completion closure, this will be called when the animation completes, and its finished will be true if the animations completed fully
+            }) { [unowned self] (finished: Bool) in
+                //the completion closure unhides the tap button so it can be tapped again
+                self.tap.hidden = false
+        }
+        
         //cycle through animations each time the button is tapped
         ++currentAnimation
         

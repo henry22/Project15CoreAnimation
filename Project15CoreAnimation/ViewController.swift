7d6cb0eaa1f94eb02c6f95177169c916216d98db
henry22 <henry811010@gmail.com>
2015-06-22 16:49:54 +0800

    That places the penguin in the middle of an iPad-sized landscape screen.

--- Project15CoreAnimation/ViewController.swift
+++ Project15CoreAnimation/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tap: UIButton!
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapped(sender: AnyObject) {
+    }
 
 }
 
